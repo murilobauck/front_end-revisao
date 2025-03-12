5d3437d0da497a76bcc9d6c1f608475063f616cc
Murilo <murilobauck2@gmail.com>
2025-03-12 00:56:04 -0300

    link das paginas

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio do formulário
+    event.preventDefault();
 
-    // Dados de login (simulação)
     const usuarioCorreto = "murilobauck";
     const senhaCorreta = "senha123";
 
-    // Captura os valores dos campos
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Validação
     if (username === usuarioCorreto && password === senhaCorreta) {
-        // Redireciona para a página de boas-vindas
         window.location.href = "home.html";
     } else {
         alert("Usuário ou senha incorretos. Tente novamente.");
