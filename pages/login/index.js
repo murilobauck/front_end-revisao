0e421b256f5a610ab8625157f831311b9950c53c
Murilo <murilobauck2@gmail.com>
2025-03-14 20:14:22 -0300

    alert login invalido

--- pages/login/index.js
+++ pages/login/index.js
@@ -12,5 +12,5 @@ document.getElementById('login-form').addEventListener('submit', function(event)
                 window.location.href = '../home/home.html';
             }, 300);
     } else {
-        alert('Usuário ou senha incorretos');
+        loginErro.textContent = `Usuário ou senha inválidos!`;
     }});
